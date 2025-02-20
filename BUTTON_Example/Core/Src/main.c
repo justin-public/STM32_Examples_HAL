bfc3c1bb411a3de1e062ccec21ac09d6bda23e25
compro <dajjong7777@gmail.com>
2025-02-20 16:32:35 +0900

    second commit

--- BUTTON_Example/Core/Src/main.c
+++ BUTTON_Example/Core/Src/main.c
@@ -5,10 +5,10 @@
   * @brief          : Main program body
   ******************************************************************************
   * @attention
-  *
+  * test ing..
   * Copyright (c) 2025 STMicroelectronics.
   * All rights reserved.
-  *
+  * 
   * This software is licensed under terms that can be found in the LICENSE file
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
